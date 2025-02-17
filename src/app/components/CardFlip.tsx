bbf5fddb2518cc55bfa9205ed7fc778b263c0fc0
the-KALU-DEV <the.kalu.dev@gmail.com>
2025-02-17 06:31:35 +0100

    Fixed the broken hero on mobile

--- src/app/components/CardFlip.tsx
+++ src/app/components/CardFlip.tsx
@@ -9,8 +9,8 @@ interface CardFlipProps {
 
 export default function CardFlip({ frontImage, backText }: CardFlipProps) {
   return (
-    <div className="group relative w-full -mt-10 h-64 md:h-72 lg:h-80">
-      <div className="relative h-full w-full rounded-lg transition-transform duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+    <div className="group relative w-full h-64 md:h-72 lg:h-80">
+      <div className="-mt-10 relative h-full w-full rounded-lg transition-transform duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
         {/* Front Side */}
         <div className="absolute inset-0 rounded-lg [backface-visibility:hidden] overflow-hidden">
           <Image
